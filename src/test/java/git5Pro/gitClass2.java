8e8040c12246c87b78571e8c370f9275b2d4dc67
Robin <bhardwajrobin771@gmail.com>
2024-04-13 22:51:52 +0530

    Commit , Apr 13, 22:51

---
+++ src/test/java/git5Pro/gitClass2.java
@@ -0,0 +1,15 @@
+package git5Pro;
+
+public class gitClass2 {
+
+	public static void main(String[] args) {
+System.out.println("Hello, world!");
+        
+        // Print multiple lines of text
+        System.out.println("This is line 1.");
+        System.out.println("This is line 2.");
+        System.out.println("This is line 2.");
+
+	}
+
+}
